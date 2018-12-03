2bb0fc3cc834393dac654f22dbaa67a5cdb540f1
Jeldrik Hanschke <admin@jhanschke.de>
2018-12-03 21:46:20 +0100

    test: add integration test for <ConferenceForm> component

--- tests/integration/components/conference-form-test.js
+++ tests/integration/components/conference-form-test.js
@@ -1,26 +1,39 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, fillIn, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | conference-form', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.set('model', {
+      name: 'enterJS 2018',
+      startDate: '2018-06-19',
+      endDate: '2018-06-22',
+      address: 'Schloßgraben 1, 64283 Darmstadt'
+    });
+  });
 
-    await render(hbs`{{conference-form}}`);
+  test('input elements are prefilled with current values of model', async function(assert) {
+    await render(hbs`<ConferenceForm @conference={{this.model}} />`);
+    assert.dom('[data-test-element="name"] input').hasValue(this.get('model.name'));
+    assert.dom('[data-test-element="start-date"] input').hasValue(this.get('model.startDate'));
+    assert.dom('[data-test-element="end-date"] input').hasValue(this.get('model.endDate'));
+    assert.dom('[data-test-element="address"] input').hasValue(this.get('model.address'));
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#conference-form}}
-        template block text
-      {{/conference-form}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('onSave event receives user input', async function(assert) {
+    this.set('saveAction', (data) => {
+      assert.step('save action');
+      assert.deepEqual(data, this.get('model'), 'filled in values are passed to save action');
+    });
+    await render(hbs`<ConferenceForm @conference={{hash}} @onSave={{saveAction}} />`);
+    await fillIn('[data-test-element="name"] input', this.get('model.name'));
+    await fillIn('[data-test-element="start-date"] input', this.get('model.startDate'));
+    await fillIn('[data-test-element="end-date"] input', this.get('model.endDate'));
+    await fillIn('[data-test-element="address"] input', this.get('model.address'));
+    await click('[data-test-button="submit"]');
+    assert.verifySteps(['save action'], 'save action has been exectued');
   });
 });
