f70d00d415521a8cf2a295be560cc4e9c738c886
Jeldrik Hanschke <admin@jhanschke.de>
2018-12-03 21:25:14 +0100

    refactor: improved format for period

--- app/models/conference.js
+++ app/models/conference.js
@@ -8,8 +8,36 @@ export default DS.Model.extend({
   endDate: DS.attr('string'),
 
   period: computed('startDate', 'endDate', function() {
-    let startDateFormatted = (new Date(this.startDate)).toLocaleString();
-    let endDateFormatted = (new Date(this.endDate)).toLocaleString();
-    return `${startDateFormatted} - ${endDateFormatted}`;
+    let { startDate: startDateString, endDate: endDatestring } = this;
+    let startDate = new Date(startDateString);
+    let endDate = new Date(endDatestring);
+
+    let dayOnly = { day: 'numeric' };
+    let monthOnly = Object.assign({ month: 'long' }, dayOnly);
+    let fullDate = Object.assign({ year: 'numeric' }, monthOnly);
+    let period = '';
+
+    if (!startDateString || !endDatestring) {
+      return null;
+    }
+
+    if (startDate.getYear() !== endDate.getYear()) {
+      period = startDate.toLocaleDateString('de-DE', fullDate);
+    }
+    else if (startDate.getMonth() !== endDate.getMonth()) {
+      period = startDate.toLocaleDateString('de-DE', monthOnly);
+    }
+    else if (startDate.getDay() !== endDate.getDay()) {
+      period = startDate.toLocaleDateString('de-DE', dayOnly).concat('.');
+    }
+
+    if (period.length > 0) {
+      period = period.concat(' - ');
+    }
+
+    return period.concat(
+      endDate.toLocaleDateString('de-DE', fullDate)
+    );
+
   })
 });
